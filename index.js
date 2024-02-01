3f4a3ada4fd183ea3b5a21cfb2d133eda31b788d
[thzthix] <seohas0428@naver.com>
2024-02-01 22:56:11 +0900

    uibuild

--- index.js
+++ index.js
@@ -30,10 +30,16 @@ app.get("/info", (request, response) => {
     `<h1>phonebook has info for ${data.length} people</h1> <br/> <h2>${date}</h2> `
   )
 })
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person)
-  })
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.post("/api/persons", (request, response) => {
@@ -72,7 +78,32 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error))
 })
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id
+  const { name, number } = request.body
+  Person.findByIdAndUpdate(id, {
+    name,
+    number,
+  })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
+})
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
